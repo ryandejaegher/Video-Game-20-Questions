bb5d1ba0883fb57b8a7f8522d7d299d47ad1d4c1
Ryan Dejaegher <dejaegher.ryan@gmail.com>
2018-05-07 21:40:05 -0600

    Initial Commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const gamecube = [
+    {
+        name: "Legend of Zelda: Wind Waker",
+        releaseDate: 2002,
+        publisher: "Nintendo"
+    },
+    {
+        name: "Metroid Prime",
+        releaseDate: 2002,
+        publisher: "Nintendo"
+    },
+    {
+        name: "Luigi's Mansion",
+        releaseDate: 2001,
+        publisher: "Nintendo"
+    },
+    {
+        name: "Killer 7",
+        releaseDate: 2005,
+        publisher: "Capcom"
+    }
+]
+
+
+
+ 
